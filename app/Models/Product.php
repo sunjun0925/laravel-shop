ef1e69bc84945b1ee97c36e13f11c193a814a99c
sunjun <15818952359@163.com>
2018-08-09 14:56:52 +0800

    用户界面 - 商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -21,5 +22,20 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    /**
+     * 拼接完整的image路径
+     * Laravel 的模型访问器会自动把下划线改为驼峰，所以 image_url 对应的就是 getImageUrlAttribute。
+     * @return mixed
+     */
+    public function getImageUrlAttribute()
+    {
+        // 如果 image 字段本身就已经是完整的 url 就直接返回
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+            return $this->attributes['image'];
+        }
+
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
